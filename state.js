f25ef634b962df7d09646f57af3b81297ff9e688
DarielVRX <rdharyel@gmail.com>
2026-02-21 16:54:14 -0600

    Create state.js

---
+++ state.js
@@ -0,0 +1,10 @@
+export const state = {
+  currentColor: '#ff0000',
+  brushSize: 2,
+  glbModel: null,
+  isDrawing: false,
+  hoveredObject: null,
+  lastClickedObject: null,
+  selectedObjects: [],
+  cameraLocked: false
+};
