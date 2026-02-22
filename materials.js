2a672a9e1a22196ba75874402c2837f641f6af40
DarielVRX <rdharyel@gmail.com>
2026-02-22 01:23:35 -0600

    Delete materials.js

--- materials.js
+++
@@ -1,9 +0,0 @@
-import { THREE } from './core.js';
-
-export const baseMaterial = new THREE.MeshStandardMaterial({color:0xaaaaaa});
-
-export const hoverMaterial = baseMaterial.clone();
-hoverMaterial.emissive.setHex(0x333333);
-
-export const lastClickedMaterial = baseMaterial.clone();
-lastClickedMaterial.emissive.setHex(0x555555);
