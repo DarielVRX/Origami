1271572ed53d2b6bf6ffcd7120db6dc15d9f5c68
DarielVRX <rdharyel@gmail.com>
2026-02-26 05:33:53 -0600

    Update generator.js

--- generator.js
+++ generator.js
@@ -168,7 +168,7 @@ function recomputeYOffsets() {
   for (let i = 1; i < rings.length; i++) {
     if (!rings[i].yOffsetAuto) continue;
     const prev = rings[i - 1];
-    rings[i].yOffset = parseFloat((prev.yOffset + prev.layers * V_STEP_BASE / Math.max(prev.scale, 0.0001)).toFixed(2));
+    rings[i].yOffset = parseFloat((prev.yOffset + prev.layers / V_STEP_BASE / Math.max(prev.scale, 0.0001)).toFixed(2));
   }
 }
 
